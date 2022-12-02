716e3b198bdbd3be30a941f28c190efb4c91c905
Adrian Zietal <zietal.adrian@gmail.com>
2022-12-02 13:09:12 +0100

    fix marquee

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,92 +1,70 @@
 const Stack = () => {
   return (
-    <section className="flex justify-center h-[300px] lg:h-[400px]">
-      <div className="w-0 md:w-[300px]">
-        <img
-          src={require("../img/coding.png")}
-          alt=""
-          className="w-0 md:w-[230px] mt-[15px] lg:mt-[72px]"
-        />
-      </div>
-      <div className="overflow-hidden">
-        <div className="h-[50px] w-[450px] relative block top-[15%] lg:top-[25%] overflow-hidden">
-          <div className="absolute block m-auto whitespace-nowrap overflow-hidden min-w-full h-full">
-            <span className="inline-block text-center pl-[100%] whitespace-nowrap min-w-full opacity-50 text-xl uppercase animate-marquee">
-              <span className="px-3">HTML</span>
-              <span className="px-3">CSS</span>
-              <span className="px-3">Responsive</span>
-              <span className="px-3">API</span>
-              <span className="px-3">React</span>
-            </span>
-          </div>
-          <div className="absolute block m-auto whitespace-nowrap overflow-hidden min-w-full h-full">
-            <span className="inline-block text-center pl-[100%] whitespace-nowrap min-w-full font-[85px] opacity-50 text-xl uppercase animate-[marquee_20s_linear_infinite_10000ms]">
-              <span className="px-3">HTML</span>
-              <span className="px-3">CSS</span>
-              <span className="px-3">Responsive</span>
-              <span className="px-3">API</span>
-              <span className="px-3">React</span>
-            </span>
-          </div>
+    <section className="mt-[80px]">
+      <div className="h-[300px] flex justify-center lg:h-[400px]">
+        <div className="w-0 md:w-[300px] h-[250px]">
+          <img
+            src={require("../img/coding.png")}
+            alt=""
+            className="w-0 md:w-[230px]"
+          />
         </div>
-        <div className="h-[50px] w-[450px] relative block top-[15%] lg:top-[25%] overflow-hidden">
-          <div className="absolute block m-auto whitespace-nowrap overflow-hidden min-w-full h-full">
-            <span className="inline-block text-center pl-[100%] whitespace-nowrap min-w-full font-[85px] opacity-50 text-xl uppercase animate-[marquee-right_20s_linear_infinite_2000ms]">
-              <span className="px-3">TypeScript</span>
-              <span className="px-3">GIT</span>
-              <span className="px-3">JavaScript</span>
-              <span className="px-3">OOP</span>
-              <span className="px-3">C++</span>
-            </span>
+        <div className="w-[400px] h-[250px] mt-3 relative flex overflow-x-hidden uppercase opacity-50">
+          <div className="py-5 animate-marquee whitespace-nowrap">
+            <span className="mx-3 text-xl">HTML</span>
+            <span className="mx-3 text-xl">CSS</span>
+            <span className="mx-3 text-xl">Responsive</span>
+            <span className="mx-3 text-xl">API</span>
+            <span className="mx-3 text-xl">React</span>
           </div>
-          <div className="absolute block m-auto whitespace-nowrap overflow-hidden min-w-full h-full">
-            <span className="inline-block text-center pl-[100%] whitespace-nowrap min-w-full font-[85px] opacity-50 text-xl uppercase animate-[marquee-right_20s_linear_infinite_12000ms]">
-              <span className="px-3">TypeScript</span>
-              <span className="px-3">GIT</span>
-              <span className="px-3">JavaScript</span>
-              <span className="px-3">OOP</span>
-              <span className="px-3">C++</span>
-            </span>
+          <div className="absolute top-0 py-5 animate-marquee2 whitespace-nowrap">
+            <span className="mx-3 text-xl">HTML</span>
+            <span className="mx-3 text-xl">CSS</span>
+            <span className="mx-3 text-xl">Responsive</span>
+            <span className="mx-3 text-xl">API</span>
+            <span className="mx-3 text-xl">React</span>
           </div>
-        </div>
-        <div className="h-[50px] w-[450px] relative block top-[15%] lg:top-[25%] overflow-hidden">
-          <div className="absolute block m-auto whitespace-nowrap overflow-hidden min-w-full h-full">
-            <span className="inline-block text-center pl-[100%] whitespace-nowrap min-w-full font-[85px] opacity-50 text-xl uppercase animate-[marquee_20s_linear_infinite_2000ms]">
-              <span className="px-3">HTML</span>
-              <span className="px-3">CSS</span>
-              <span className="px-3">Responsive</span>
-              <span className="px-3">API</span>
-              <span className="px-3">React</span>
-            </span>
+          <div className="absolute top-12 py-5 animate-[marquee-right_10s_linear_infinite_2000ms] whitespace-nowrap">
+            <span className="mx-3 text-xl">TypeScript</span>
+            <span className="mx-3 text-xl">GIT</span>
+            <span className="mx-3 text-xl">JavaScript</span>
+            <span className="mx-3 text-xl">OOP</span>
+            <span className="mx-3 text-xl">C++</span>
           </div>
-          <div className="absolute block m-auto whitespace-nowrap overflow-hidden min-w-full h-full">
-            <span className="inline-block text-center pl-[100%] whitespace-nowrap min-w-full font-[85px] opacity-50 text-xl uppercase animate-[marquee_20s_linear_infinite_12000ms]">
-              <span className="px-3">HTML</span>
-              <span className="px-3">CSS</span>
-              <span className="px-3">Responsive</span>
-              <span className="px-3">API</span>
-              <span className="px-3">React</span>
-            </span>
+          <div className="absolute top-12 py-5 animate-[marquee2-right_10s_linear_infinite_2000ms] whitespace-nowrap">
+            <span className="mx-3 text-xl">TypeScript</span>
+            <span className="mx-3 text-xl">GIT</span>
+            <span className="mx-3 text-xl">JavaScript</span>
+            <span className="mx-3 text-xl">OOP</span>
+            <span className="mx-3 text-xl">C++</span>
           </div>
-        </div>
-        <div className="h-[50px] w-[450px] relative block top-[15%] lg:top-[25%] overflow-hidden">
-          <div className="absolute block m-auto whitespace-nowrap overflow-hidden min-w-full h-full">
-            <span className="inline-block text-center pl-[100%] whitespace-nowrap min-w-full font-[85px] opacity-50 text-xl uppercase animate-marquee-right">
-              <span className="px-3">TypeScript</span>
-              <span className="px-3">GIT</span>
-              <span className="px-3">JavaScript</span>
-              <span className="px-3">OOP</span>
-              <span className="px-3">C++</span>
-            </span>
+          <div className="absolute top-24 py-5 animate-[marquee_10s_linear_infinite_2000ms] whitespace-nowrap">
+            <span className="mx-3 text-xl">HTML</span>
+            <span className="mx-3 text-xl">CSS</span>
+            <span className="mx-3 text-xl">Responsive</span>
+            <span className="mx-3 text-xl">API</span>
+            <span className="mx-3 text-xl">React</span>
           </div>
-          <div className="absolute block m-auto whitespace-nowrap overflow-hidden min-w-full h-full">
-            <span className="inline-block text-center pl-[100%] whitespace-nowrap min-w-full font-[85px] opacity-50 text-xl uppercase animate-[marquee-right_20s_linear_infinite_10000ms]">
-              <span className="px-3">TypeScript</span>
-              <span className="px-3">GIT</span>
-              <span className="px-3">JavaScript</span>
-              <span className="px-3">OOP</span>
-              <span className="px-3">C++</span>
-            </span>
+          <div className="absolute top-24 py-5 animate-[marquee2_10s_linear_infinite_2000ms] whitespace-nowrap">
+            <span className="mx-3 text-xl">HTML</span>
+            <span className="mx-3 text-xl">CSS</span>
+            <span className="mx-3 text-xl">Responsive</span>
+            <span className="mx-3 text-xl">API</span>
+            <span className="mx-3 text-xl">React</span>
+          </div>
+          <div className="absolute top-36 py-5 animate-marquee-right whitespace-nowrap">
+            <span className="mx-3 text-xl">TypeScript</span>
+            <span className="mx-3 text-xl">GIT</span>
+            <span className="mx-3 text-xl">JavaScript</span>
+            <span className="mx-3 text-xl">OOP</span>
+            <span className="mx-3 text-xl">C++</span>
+          </div>
+          <div className="absolute top-36 py-5 animate-marquee2-right whitespace-nowrap">
+            <span className="mx-3 text-xl">TypeScript</span>
+            <span className="mx-3 text-xl">GIT</span>
+            <span className="mx-3 text-xl">JavaScript</span>
+            <span className="mx-3 text-xl">OOP</span>
+            <span className="mx-3 text-xl">C++</span>
           </div>
         </div>
       </div>

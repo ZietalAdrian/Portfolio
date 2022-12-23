c77f0dde01eb03d795a78c9137bd7b6f48a38819
Adrian Zietal <zietal.adrian@gmail.com>
2022-12-23 13:37:00 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { HiEnvelope } from "react-icons/hi2";
 import { useInView } from "react-intersection-observer";
 import { useRef } from "react";
+import { Analytics } from '@vercel/analytics/react';
 
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -28,6 +29,7 @@ function App() {
   };
 
   return (
+    <>
     <div className="font-bodoni text-gray-100 relative overflow-hidden">
       <div
         ref={about}
@@ -100,6 +102,8 @@ function App() {
         ⓒ 2022 Adrian Ziętal | All Rights Reserved
       </footer>
     </div>
+    <Analytics />
+    </>
   );
 }
 
